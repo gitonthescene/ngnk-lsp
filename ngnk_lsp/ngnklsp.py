1104fb4a0e864586a9e434b17d553345aa7e7b35
Douglas Mennella <douglas.mennella@gmail.com>
2024-01-30 17:23:55 +0900

    Report tabs in source.

--- ngnk_lsp/ngnklsp.py
+++ ngnk_lsp/ngnklsp.py
@@ -4,6 +4,7 @@ import logging
 from pygls.server import LanguageServer
 from lsprotocol import types as lsp
 import ngnk_lsp.semantic as semantic
+import re
 
 logging.basicConfig(filename='pygls.log', filemode='w', level=logging.DEBUG)
 
@@ -36,6 +37,70 @@ def semantic_tokens(ls: NgnkLanguageServer, params: lsp.SemanticTokensParams):
 
     data = semantic.get_semantic_tokens(bytearray(doc.source, "utf-8"))
     return lsp.SemanticTokens(data=data)
+
+
+@ngnk_server.feature(lsp.TEXT_DOCUMENT_DID_CHANGE)
+def did_change(ls, params: lsp.DidChangeTextDocumentParams):
+    """Text document did change notification."""
+    _validate(ls, params)
+
+
+@ngnk_server.feature(lsp.TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls, params: lsp.DidOpenTextDocumentParams):
+    """Text document did open notification."""
+    ls.show_message("Text Document Did Open")
+    _validate(ls, params)
+
+
+@ngnk_server.feature(
+    lsp.TEXT_DOCUMENT_DIAGNOSTIC,
+    lsp.DiagnosticOptions(
+        identifier="ngnkLSPServer",
+        inter_file_dependencies=True,
+        workspace_diagnostics=True,
+    ),
+)
+def text_document_diagnostic(
+    params: lsp.DocumentDiagnosticParams,
+) -> lsp.DocumentDiagnosticReport:
+    """Return diagnostic report."""
+    document = ngnk_server.workspace.get_document(params.text_document.uri)
+    return lsp.RelatedFullDocumentDiagnosticReport(
+        items=_validate_doc(document),
+        kind=lsp.DocumentDiagnosticReportKind.Full,
+    )
+
+
+def _validate(ls, params):
+    ls.show_message_log("Validating ngn/k source...")
+
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+
+    diagnostics = _validate_doc(text_doc)
+
+    ls.publish_diagnostics(text_doc.uri, diagnostics)
+
+
+def _validate_doc(doc):
+    """Validate json file."""
+    diagnostics = []
+
+    TABS = re.compile(r'\t+')
+    for line, txt in enumerate(doc.lines):
+        for m in TABS.finditer(txt):
+            start, end = m.span()
+            d = lsp.Diagnostic(
+                range=lsp.Range(
+                    start=lsp.Position(line=line, character=start),
+                    end=lsp.Position(line=line, character=end),
+                ),
+                message="tabs forbidden",
+                source=type(ngnk_server).__name__,
+            )
+
+            diagnostics.append(d)
+
+    return diagnostics
 
 
 def main():
